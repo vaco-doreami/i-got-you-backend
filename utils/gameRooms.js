04715d2b4bd76bb46cac7463e653093ab1572f3e
jaeho seo <jaehoseo@jaehoui-MacBookPro.local>
2022-06-05 19:54:16 +0900

    feat: 방 생성 및 방 참여

---
+++ utils/gameRooms.js
@@ -0,0 +1,29 @@
+const rooms = {};
+
+exports.createRoom = (socketId, player) => {
+  rooms[socketId] = {
+    [socketId]: { ...player },
+  };
+};
+
+exports.joinRoom = (roomId, socketId, player) => {
+  rooms[roomId] = {
+    ...rooms[roomId],
+    [socketId]: { ...player },
+  };
+};
+
+exports.getRoomPlayers = (roomId) => {
+  const gameRoom = rooms[roomId];
+  const players = [];
+
+  for (const player in gameRoom) {
+    players.push(player);
+  }
+
+  return players;
+};
+
+exports.getAllRooms = () => {
+  return rooms;
+};
---
+++ utils/socketio.js
@@ -0,0 +1,74 @@
+const socketio = require("socket.io");
+
+const {
+  createRoom,
+  joinRoom,
+  getRoomPlayers,
+  getAllRooms,
+} = require("./gameRooms");
+
+module.exports = (server) => {
+  const io = socketio(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("enter-roomList", () => {
+      const rooms = getAllRooms();
+
+      socket.join("roomList");
+      socket.broadcast.to("roomList").emit("send-rooms", rooms);
+    });
+
+    socket.on(
+      "create-room",
+      ({ nickname, job, characterType, coordinateX, coordinateY }) => {
+        const socketId = socket.id;
+
+        const player = {
+          isHost: true,
+          nickname,
+          job,
+          characterType,
+          coordinateX,
+          coordinateY,
+        };
+
+        createRoom(socketId, player);
+        socket.join(socketId);
+
+        const rooms = getAllRooms();
+
+        socket.broadcast.to("roomList").emit("send-rooms", rooms);
+      }
+    );
+
+    socket.on(
+      "join-room",
+      ({ roomId, nickname, job, characterType, coordinateX, coordinateY }) => {
+        const socketId = socket.id;
+
+        socket.leave("roomList");
+
+        const player = {
+          isHost: false,
+          nickname,
+          job,
+          characterType,
+          coordinateX,
+          coordinateY,
+        };
+
+        joinRoom(roomId, socketId, player);
+        socket.join(roomId);
+
+        const players = getRoomPlayers(roomId);
+
+        socket.broadcast.to(roomId).emit("receive-player", players);
+      }
+    );
+  });
+};
