1865cd899fc4298310e090d325e6a2068d1bce93
jaeho seo <jaehoseo@jaehoui-MacBookPro.local>
2022-06-07 19:09:14 +0900

    feature: 방 생성, 방 참여

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+require("dotenv").config();
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const createError = require("http-errors");
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
 
-var app = express();
+const app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
 
-app.use(logger("dev"));
+const webSocket = require("./utils/socketio");
+webSocket(server);
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));
@@ -30,7 +27,12 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render("error");
+  res.json({
+    status: res.status,
+    message: "error",
+  });
 });
 
-module.exports = app;
+server.listen(port);
+server.on("error", error => console.error(error));
+server.on("listening", () => console.log(`listening on port ${port}`));
